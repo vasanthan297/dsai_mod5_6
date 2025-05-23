7c3043781025766e8ad3b6e3234614429b6c32ea
unknown <vasanthan297@gmail.com>
2025-05-23 11:16:11 +0800

    updated

--- app.py
+++ app.py
@@ -1,30 +1,40 @@
 #gemini
 
-from flask import Flask, request, render_template
+from flask import Flask,request,render_template
 import google.generativeai as genai
-from openai import OpenAI
 import os
-from markdown2 import Markdown
+import sqlite3
+import datetime
 
+gemini_api_key = os.getenv("gemini_api_key")
 
-# Configure Gemini
-genai.configure(api_key=os.environ["GEMINI_KEY"])
+genai.configure(api_key=gemini_api_key)
+model = genai.GenerativeModel("gemini-2.0-flash")
 
-# Configure OpenAI
-client = OpenAI(api_key=os.environ["OPENAI_KEY"])
-
-# Configure Gemini model
-# model = genai.GenerativeModel("gemini-2.0-flash")
-model = genai.GenerativeModel("gemini-2.5-flash-preview-04-17")
-
-# Flask
 app = Flask(__name__)
 
-@app.route("/", methods=["GET", "POST"])
+first_time = 1
+
+@app.route("/",methods=["GET","POST"])
 def index():
     return(render_template("index.html"))
 
-# Gemini
+@app.route("/main",methods=["GET","POST"])
+def main():
+    global first_time
+    if first_time==1:
+        q = request.form.get("q")
+        print(q)
+        t = datetime.datetime.now()
+        conn = sqlite3.connect('user.db')
+        c = conn.cursor()
+        c.execute("insert into users(name,timestamp) values(?,?)",(q,t))
+        conn.commit()
+        c.close()
+        conn.close()
+        first_time=0
+    return(render_template("main.html"))
+
 @app.route("/gemini",methods=["GET","POST"])
 def gemini():
     return(render_template("gemini.html"))
@@ -34,27 +44,42 @@ def gemini_reply():
     q = request.form.get("q")
     print(q)
     r = model.generate_content(q)
-    r = r.text
-    return(render_template("gemini_reply.html",r=r))
+    return(render_template("gemini_reply.html",r=r.text))
 
-# OpenAI
-@app.route("/openai",methods=["GET","POST"])
-def openai():
-    return(render_template("openai.html"))
+@app.route("/paynow",methods=["GET","POST"])
+def paynow():
+    return(render_template("paynow.html"))
 
-@app.route("/openai_reply",methods=["GET","POST"])
-def openai_reply():
-    q = request.form.get("q")
-    response = client.chat.completions.create(
-      model="gpt-4o",
-      messages=[{"role": "user", "content": q}]
-    )
-    r = response.choices[0].message.content
+@app.route("/user_log",methods=["GET","POST"])
+def user_log():
+    #read
+    conn = sqlite3.connect('user.db')
+    c = conn.cursor()
+    c.execute("select * from users")
+    r=""
+    for row in c:
+        print(row)
+        r= r+str(row)
+    c.close()
+    conn.close()
+    return(render_template("user_log.html",r=r))
 
-    markdowner = Markdown()
-    formatted_response = markdowner.convert(r)
-   #return(render_template("openai_reply.html",r=r))
-    return(render_template("openai_reply.html",r=formatted_response))
+@app.route("/delete_log",methods=["GET","POST"])
+def delete_log():
+    conn = sqlite3.connect('user.db')
+    c = conn.cursor()
+    c.execute("delete from users")
+    conn.commit()
+    c.close()
+    conn.close()
+    return(render_template("delete_log.html"))
+
+@app.route("/logout",methods=["GET","POST"])
+def logout():
+    global first_time
+    first_time = 1
+    return(render_template("index.html"))
 
 if __name__ == "__main__":
     app.run()
+
